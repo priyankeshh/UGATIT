585fa0be6bd54871efcde398ef077e6185c2fab3
Hridaya Sharma <53822171+hridaya14@users.noreply.github.com>
2025-10-25 17:21:51 +0530

    Update train.py

--- configs/train.py
+++ configs/train.py
@@ -17,11 +17,11 @@ _CN.resume_ckpt_path = ""
 # ======================================================
 # Training Parameters
 # ======================================================
-_CN.iterations = 1000000                 # total training iterations
+_CN.iterations = 50000                 # total training iterations
 _CN.batch_size = 2                      # batch size
 _CN.print_freq = 100                   # print image frequency
-_CN.save_freq = 100000                  # model save frequency
-_CN.val_freq = 10000
+_CN.save_freq = 10000                  # model save frequency
+_CN.val_freq = 5000
 _CN.decay_flag = True                   # enable learning rate decay
 _CN.num_workers = 4
 
